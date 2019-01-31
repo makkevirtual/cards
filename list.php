8c288eee43b4bed83a5bcf8a66fb4b3b40d2c1c1
Markus <markus.hakala@kolumbus.fi>
2019-01-31 13:59:17 +0200

    modified list.php code and added SQL commands in database

--- list.php
+++ list.php
@@ -1,11 +1,29 @@
-<html>
- <head>
- <meta charset="UTF-8">
- <title>
- Empty list
- </title>
- </head>
- <body>
- <h1>Empty list</h1>
- </body>
-</html>
+<?php
+session_start();
+if($_SESSION['username'] == 'username') {
+ include "connect.php";
+ print("<html>");
+ $conn = connect_db();
+ $sql = "SELECT * FROM Addresses";
+ $result = mysqli_query($conn, $sql);
+ if (mysqli_num_rows($result) > 0) {
+ // output data of each row
+ while($row = mysqli_fetch_assoc($result)) {
+ echo $row["id"] . " ";
+ echo $row["name"] . " ";
+ echo $row["street_address"] . " ";
+ echo $row["zip"] . " ";
+ echo $row["city"] . " ";
+ echo $row["state"] . " ";
+ echo $row["country"] . " ";
+ print("<br>");
+ }
+ } else {
+ echo "0 results";
+ }
+ print("</html>");
+ mysqli_close($conn);
+} else {
+ print("No access.");
+}
+?>
