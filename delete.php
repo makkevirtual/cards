ddfdab2585bf832a8da569c39252f0551b7035ab
Markus <markus.hakala@kolumbus.fi>
2019-02-28 12:24:00 +0200

    delete function to list and delete.php fiel

--- delete.php
+++ delete.php
@@ -4,8 +4,8 @@ if($_SESSION['username'] == 'username') {
  include "connect.php";
  print("<html>");
  $conn = connect_db();
+// delete with id
  $sql = "DELETE FROM Addresses WHERE id=" . $_GET['id'];
- // print $sql;
  $result = mysqli_query($conn, $sql);
  print("Delete completed.");
  print("</html>");
